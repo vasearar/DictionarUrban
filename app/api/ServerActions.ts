4fd622a447a0afc84317517fce5870cff711145e
Prodius Cătălin <prodius345@gmail.com>
2024-04-03 22:43:41 +0300

    Recaptcha validation on server

---
+++ app/api/ServerActions.ts
@@ -0,0 +1,14 @@
+"use server"
+
+import axios from "axios"
+
+export async function verifyCaptcha(token: string | null) {
+  const res = await axios.post(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`
+  )
+  if (res.data.success) {
+    return true
+  } else {
+    throw new Error("Failed Captcha")
+  }
+}
--- app/define/page.tsx
+++ app/define/page.tsx
@@ -1,29 +1,36 @@
 'use client';
-import React, { useState } from "react"
+import React, { FormEvent, useState } from "react"
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import ReCAPTCHA from "react-google-recaptcha";
+import { verifyCaptcha } from "../api/ServerActions";
+
 const page = () => {
 	const session = useSession();
   const [isMessageSent, setMessageSent] = useState(false);
-	const [captcha, setCaptcha] = useState();
+	const [captcha, setCaptcha] = useState<string | null>();
 
-	const onSubmit = (e : React.FormEvent) => {
+	async function onSubmit(e: FormEvent){
 		e.preventDefault();
 		if (captcha){
-			handleSubmit(e);
-		} else {
+			try{
+				if(await verifyCaptcha(captcha)){
+					handleSubmit(e);
+				}
+			} catch (error){
+				console.error("Error verifying captcha:", error);
+			}
+	} else {
 			console.log("ReCAPTCHA not verified");
-		}
+	}
 	}
 	
-	async function handleSubmit(e: React.FormEvent<Element>) {
+	async function handleSubmit(e: React.FormEvent) {
 		e.preventDefault();	
-		const target = e.currentTarget as HTMLFormElement;
-
-		const word = (target.elements.namedItem("word") as HTMLInputElement);
-		const definition = target.elements.namedItem("definition") as HTMLInputElement;
-		const exampleOfUsing = target.elements.namedItem("exampleOfUsing") as HTMLInputElement;
+		const target = e.target as HTMLFormElement;
+		const word = target?.elements.namedItem("word") as HTMLInputElement;
+		const definition = target?.elements.namedItem("definition") as HTMLInputElement;
+		const exampleOfUsing = target?.elements.namedItem("exampleOfUsing") as HTMLInputElement;
 
 		const date = new Date();
 
@@ -34,7 +41,7 @@ const page = () => {
 		const options : CustomDateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC', locale: 'ro' };
 
 		const data = {
-			word: word.value.toLowerCase(),
+			word: word?.value.toLowerCase(),
 			definition: definition.value,
 			exampleOfUsing: exampleOfUsing.value,
 			username: session?.data?.user?.name,
@@ -59,11 +66,11 @@ const page = () => {
 			setMessageSent(true);
 		} catch (error) {
 			console.log(
-				"There was a problem with the fetch operation " + error.message
+				"There was a problem with the fetch operation: ", error
 			);
 		}
 		document.querySelector("form")?.reset();
-		captcha.reset();
+		grecaptcha.reset();
 	}
 
   return (
@@ -72,13 +79,15 @@ const page = () => {
       <h1 className='text-4xl mb-16'>Șansa ta de a adăuga un cuvânt sau expresie în dicționar</h1>
       <form onSubmit={onSubmit} className='rounded-lg bg-neutral-600 max-w-[806px] relative h-fit px-7 py-6 shadow-lg'>
         <p>*Distribuiți definiții pe care alte persoane le vor găsi folositoare și nu postați niciodată texte înjositoare sau informații personale ale oamenilor.</p>
-        <input className='outline-none bg-transparent text-2xl mt-6 mb-2 border-2 rounded-lg p-2 w-full' type='text' name='word' id="word" required placeholder='Cuvântul sau expresia' />
+        <input className='outline-none bg-transparent text-2xl mt-6 mb-2 border-2 rounded-lg p-2 w-full' maxLength={35} type='text' name='word' id="word" required placeholder='Cuvântul sau expresia' />
         <p className='text-xs'>*scrie pentru o auditorie largă</p>
-        <textarea className='bg-transparent w-full outline-none border-2 rounded-lg p-2 mt-2 h-36 resize-none' maxLength="450" name="definition" id="definition" required placeholder='Scrie aici definiția ta...'></textarea>
-        <textarea className='bg-transparent w-full outline-none border-2 rounded-lg p-2 my-4 h-[4.3rem] resize-none' maxLength="200" name='exampleOfUsing' id="exampleOfUsing" required placeholder='Scrie un exemplu cum se folosește într-un enunț'></textarea>
-				<ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY} onChange={setCaptcha} className="flex justify-center" />
+        <textarea className='bg-transparent w-full outline-none border-2 rounded-lg p-2 mt-2 h-36 resize-none' maxLength={450} name="definition" id="definition" required placeholder='Scrie aici definiția ta...'></textarea>
+        <textarea className='bg-transparent w-full outline-none border-2 rounded-lg p-2 my-4 h-[4.3rem] resize-none' maxLength={200} name='exampleOfUsing' id="exampleOfUsing" required placeholder='Scrie un exemplu cum se folosește într-un enunț'></textarea>
+				<ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!} onChange={setCaptcha} className="flex justify-center" />
         <button className='w-full h-fit bg-green-400 text-2xl font-semibold text-black py-2 rounded-lg mt-4 hover:bg-green-500' type="submit">Adaugă</button>
-				<Link href="/"><img src="/arrowBack.svg" className="brightness-200 absolute -bottom-11 left-0" /></Link>
+				<Link href="/"><svg className="brightness-200 absolute -bottom-11 left-0" xmlns="http://www.w3.org/2000/svg" width="34" height="30" viewBox="0 0 34 30" fill="none">
+  				<path d="M2.41663 15L14.9166 2.5M2.41663 15L14.9166 27.5M2.41663 15L31.5833 15" stroke="white" strokeWidth="4.16667" strokeLinecap="round" strokeLinejoin="round"/>
+				</svg></Link>
       </form>
     </div>
 		</>
