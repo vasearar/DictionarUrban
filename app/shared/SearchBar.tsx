e167d76476301f3deb40f131b49da3c763d9fece
vasearar <prodius345@gmail.com>
2024-09-14 11:11:05 +0300

    Accessibility improvement

--- app/shared/SearchBar.tsx
+++ app/shared/SearchBar.tsx
@@ -46,8 +46,7 @@ const SearchBar = () => {
           className={`mydropshadow my-auto rounded-sm border-mygray bg-mywhite border-solid border-2 flex items-center px-4 py-[10px] gap-2 h-8 md:h-[48px] w-full lg:w-44 2xl:w-[20vw] relative`}
           onSubmit={handleSearch}
           >
-          <button type='submit'>
-          <span className="hidden">Submit</span>
+          <button type='submit' aria-label="Submit form">
           <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M1.82324 3.52173H0.125V8.61683H1.82337V10.3152H3.52173V12.0136H8.9565V10.3152H10.6549V12.0136H12.3532V13.7119H14.0516V15.4103H15.75V13.7119H14.0516V12.0136H12.3532V10.3152H10.6549V8.61683H12.3532V3.52173H10.6549V1.82337H8.9565V0.125H3.52173V1.8125H8.95638V3.51087H10.6547V8.60597H8.95638V10.3043H3.52161V8.60597H1.82324V3.52173ZM1.82337 3.51087H3.52161V1.82337H1.82337V3.51087Z" fill="#202020"/>
           </svg>
