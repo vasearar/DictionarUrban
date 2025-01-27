e26c86b4fd8f6ca86d53d87f2c27378a33bdcbe5
vasearar <prodius345@gmail.com>
2025-01-27 20:18:11 +0200

    anonym logare

--- app/confings/auth.ts
+++ app/confings/auth.ts
@@ -12,10 +12,50 @@ export const authConfig: AuthOptions = {
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID!,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
-    })
-  ],
+    }),
+    CredentialsProvider({
+      id: "anonym",
+      name: "Conectare anonimă",
+      credentials: {
+        username: { label: "Username", type: "text" },
+      },
+      async authorize(credentials, req) {
+        const randomID = Math.random().toString(36).substring(2, 15);
+        const fakeEmail = `anon_${randomID}@no-reply.localhost`;
 
+        const user = {
+          id: randomID,
+          name: "Musafir",
+          email: fakeEmail,
+        };
+
+        if (!user) return null;
+
+        return user;
+      },
+    }),
+  ],
   pages: {
-    signIn: '/signIn'
+    signIn: '/signIn',
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user = {
+          ...session.user,
+          id: token.sub,
+          email: token.email,
+        };
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.sub = user.id;
+        token.email = user.email;
+        token.name = user.name;
+      }
+      return token;
+    }
   }
-}
+};
